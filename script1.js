f0ce3585a44acc84c81520456a2e46fb4902fd86
Youness KOUROU <91493780+yokourou@users.noreply.github.com>
2025-01-02 16:05:12 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -2,21 +2,17 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 function redirectToPage() {
-    window.location.href = "destination.html"; // Spécifiez l'URL de destination
+    window.location.href = "destination.html"; 
 };
 
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
         define(['jquery'], factory);
     } else if ((typeof module === 'undefined' ? 'undefined' : _typeof(module)) === 'object' && module.exports) {
         // Node/CommonJS
         module.exports = function (root, jQuery) {
             if (jQuery === undefined) {
-                // require('jQuery') returns a factory that requires window to
-                // build a jQuery instance, we normalize how we use modules
-                // that require this pattern but the window provided is a noop
-                // if it's defined (how jquery works)
+                
                 if (typeof window !== 'undefined') {
                     jQuery = require('jquery');
                 } else {
@@ -27,24 +23,17 @@ function redirectToPage() {
             return jQuery;
         };
     } else {
-        // Browser globals
         factory(jQuery);
     }
 })(function ($) {
     $.fn.tilt = function (options) {
 
-        /**
-         * RequestAnimationFrame
-         */
+        
         var requestTick = function requestTick() {
             if (this.ticking) return;
             requestAnimationFrame(updateTransforms.bind(this));
             this.ticking = true;
         };
-
-        /**
-         * Bind mouse movement evens on instance
-         */
         var bindEvents = function bindEvents() {
             var _this = this;
             $(this).on('mousemove', mouseMove);
@@ -53,9 +42,6 @@ function redirectToPage() {
             if (this.settings.glare) $(window).on('resize', updateGlareSize.bind(_this));
         };
 
-        /**
-         * Set transition only on mouse leave and mouse enter so it doesn't influence mouse move transforms
-         */
         var setTransition = function setTransition() {
             var _this2 = this;
 
@@ -68,22 +54,14 @@ function redirectToPage() {
             }, this.settings.speed);
         };
 
-        /**
-         * When user mouse enters tilt element
-         */
         var mouseEnter = function mouseEnter(event) {
             this.ticking = false;
             $(this).css({ 'will-change': 'transform' });
             setTransition.call(this);
 
-            // Trigger change event
             $(this).trigger("tilt.mouseEnter");
         };
 
-        /**
-         * Return the x,y position of the mouse on the tilt element
-         * @returns {{x: *, y: *}}
-         */
         var getMousePositions = function getMousePositions(event) {
             if (typeof event === "undefined") {
                 event = {
@@ -94,31 +72,19 @@ function redirectToPage() {
             return { x: event.pageX, y: event.pageY };
         };
 
-        /**
-         * When user mouse moves over the tilt element
-         */
         var mouseMove = function mouseMove(event) {
             this.mousePositions = getMousePositions(event);
             requestTick.call(this);
         };
 
-        /**
-         * When user mouse leaves tilt element
-         */
         var mouseLeave = function mouseLeave() {
             setTransition.call(this);
             this.reset = true;
             requestTick.call(this);
 
-            // Trigger change event
             $(this).trigger("tilt.mouseLeave");
         };
 
-        /**
-         * Get tilt values
-         *
-         * @returns {{x: tilt value, y: tilt value}}
-         */
         var getValues = function getValues() {
             var width = $(this).outerWidth();
             var height = $(this).outerHeight();
@@ -126,26 +92,17 @@ function redirectToPage() {
             var top = $(this).offset().top;
             var percentageX = (this.mousePositions.x - left) / width;
             var percentageY = (this.mousePositions.y - top) / height;
-            // x or y position inside instance / width of instance = percentage of position inside instance * the max tilt value
             var tiltX = (this.settings.maxTilt / 2 - percentageX * this.settings.maxTilt).toFixed(2);
             var tiltY = (percentageY * this.settings.maxTilt - this.settings.maxTilt / 2).toFixed(2);
-            // angle
             var angle = Math.atan2(this.mousePositions.x - (left + width / 2), -(this.mousePositions.y - (top + height / 2))) * (180 / Math.PI);
-            // Return x & y tilt values
             return { tiltX: tiltX, tiltY: tiltY, 'percentageX': percentageX * 100, 'percentageY': percentageY * 100, angle: angle };
         };
-
-        /**
-         * Update tilt transforms on mousemove
-         */
         var updateTransforms = function updateTransforms() {
             this.transforms = getValues.call(this);
 
             if (this.reset) {
                 this.reset = false;
                 $(this).css('transform', 'perspective(' + this.settings.perspective + 'px) rotateX(0deg) rotateY(0deg)');
-
-                // Rotate glare if enabled
                 if (this.settings.glare) {
                     this.glareElement.css('transform', 'rotate(180deg) translate(-50%, -50%)');
                     this.glareElement.css('opacity', '0');
@@ -154,39 +111,30 @@ function redirectToPage() {
                 return;
             } else {
                 $(this).css('transform', 'perspective(' + this.settings.perspective + 'px) rotateX(' + (this.settings.disableAxis === 'x' ? 0 : this.transforms.tiltY) + 'deg) rotateY(' + (this.settings.disableAxis === 'y' ? 0 : this.transforms.tiltX) + 'deg) scale3d(' + this.settings.scale + ',' + this.settings.scale + ',' + this.settings.scale + ')');
-
-                // Rotate glare if enabled
                 if (this.settings.glare) {
+                    
                     this.glareElement.css('transform', 'rotate(' + this.transforms.angle + 'deg) translate(-50%, -50%)');
                     this.glareElement.css('opacity', '' + this.transforms.percentageY * this.settings.maxGlare / 100);
                 }
             }
 
-            // Trigger change event
             $(this).trigger("change", [this.transforms]);
 
             this.ticking = false;
         };
 
-        /**
-         * Prepare elements
-         */
         var prepareGlare = function prepareGlare() {
             var glarePrerender = this.settings.glarePrerender;
 
-            // If option pre-render is enabled we assume all html/css is present for an optimal glare effect.
             if (!glarePrerender)
                 // Create glare element
                 $(this).append('<div class="js-tilt-glare"><div class="js-tilt-glare-inner"></div></div>');
 
-            // Store glare selector if glare is enabled
             this.glareElementWrapper = $(this).find(".js-tilt-glare");
             this.glareElement = $(this).find(".js-tilt-glare-inner");
 
-            // Remember? We assume all css is already set, so just return
             if (glarePrerender) return;
 
-            // Abstracted re-usable glare styles
             var stretch = {
                 'position': 'absolute',
                 'top': '0',
@@ -195,13 +143,10 @@ function redirectToPage() {
                 'height': '100%'
             };
 
-            // Style glare wrapper
             this.glareElementWrapper.css(stretch).css({
                 'overflow': 'hidden',
                 'pointer-events': 'none'
             });
-
-            // Style glare element
             this.glareElement.css({
                 'position': 'absolute',
                 'top': '50%',
@@ -215,9 +160,7 @@ function redirectToPage() {
             });
         };
 
-        /**
-         * Update glare on resize
-         */
+
         var updateGlareSize = function updateGlareSize() {
             this.glareElement.css({
                 'width': '' + $(this).outerWidth() * 2,
@@ -225,9 +168,6 @@ function redirectToPage() {
             });
         };
 
-        /**
-         * Public methods
-         */
         $.fn.tilt.destroy = function () {
             $(this).each(function () {
                 $(this).find('.js-tilt-glare').remove();
@@ -258,17 +198,8 @@ function redirectToPage() {
             });
         };
 
-        /**
-         * Loop every instance
-         */
         return this.each(function () {
             var _this4 = this;
-
-            /**
-             * Default settings merged with user settings
-             * Can be set trough data attributes or as parameter.
-             * @type {*}
-             */
             this.settings = $.extend({
                 maxTilt: $(this).is('[data-tilt-max]') ? $(this).data('tilt-max') : 20,
                 perspective: $(this).is('[data-tilt-perspective]') ? $(this).data('tilt-perspective') : 300,
@@ -312,4 +243,3 @@ function redirectToPage() {
 
     return true;
 });
-//# sourceMappingURL=tilt.jquery.js.map
